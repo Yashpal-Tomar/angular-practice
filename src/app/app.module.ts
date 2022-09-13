0e44e621a8ac3216a1e2c995c15ee13b95a0d78c
Yashpal Tomar <yashutomar699@gmail.com>
2022-09-13 20:30:44 +0530

    login page done

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { PublicModule } from './public/public.module' ;
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PublicModule
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
